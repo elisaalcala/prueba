1e89f3b30132553023bd2c2bfacdf654c99bcd7e
elisaalcala <elisaalcgue@gmail.com>
2024-05-31 14:40:00 +0200

    fix pom.xml to db docker

--- src/test/java/com/app/alcala/web/controller/AlcalappControllerTest.java
+++ src/test/java/com/app/alcala/web/controller/AlcalappControllerTest.java
@@ -14,8 +14,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
