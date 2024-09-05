075f58548315190f76cf9c905d95de3e0e390264
elisaalcala <102151829+elisaalcala@users.noreply.github.com>
2024-09-05 17:37:09 +0200

    Update ReleaseAndProjectTest.java

--- src/test/java/com/app/alcala/selenium/ReleaseAndProjectTest.java
+++ src/test/java/com/app/alcala/selenium/ReleaseAndProjectTest.java
@@ -59,13 +59,15 @@ public class ReleaseAndProjectTest {
     @BeforeEach
     public void setUpTest() {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        options.addArguments("--allow-insecure-localhost");
-        options.addArguments("--disable-gpu");
+        options.addArguments("--headless=new"); // Nueva opción para mejor rendimiento
+	options.addArguments("--allow-insecure-localhost");
+	options.addArguments("--disable-gpu");
 	options.addArguments("--window-size=1920,1080");
+	options.addArguments("--no-sandbox"); // Útil en entornos de CI/CD
+	options.addArguments("--disable-dev-shm-usage");
 
         driver = new ChromeDriver(options);
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(60));
         
     }
 
